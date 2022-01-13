6ba7239f429f52bcd0e919f3700435462c850a6c
Max_M <m.myshko00@gmail.com>
2022-01-13 17:52:46 +0200

    Update

--- vars/buildCall.groovy
+++ vars/buildCall.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
-def call(String job = "Downstream") {
-  build job: ${job}, parameters: [booleanParam(name: 'isTrue', value: params.isTrue)]
+def call() {
+  build job: "Downstream", parameters: [booleanParam(name: 'isTrue', value: params.isTrue)]
 }
